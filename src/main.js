3a8e113bc35d2448ac5c7e6a39541f1f41401dea
luluxia <306924625@qq.com>
2020-05-04 11:02:32 +0800

    删除没用的模块导入

--- src/main.js
+++ src/main.js
@@ -6,22 +6,9 @@ import store from './store'
 import './assets/iconfont'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueTippy, { TippyComponent } from "vue-tippy";
-import Vtip from 'vtip'
-import 'vtip/lib/index.min.css'
-
 
 Vue.config.productionTip = false
-
-Vue.use(Vtip.directive)
 Vue.use(VueAxios, axios)
-Vue.use(VueTippy, {
-  animateFill: false,
-  animation : 'fade',
-  trigger : 'click',
-  arrow : true
-});
-Vue.component("tippy", TippyComponent);
 
 new Vue({
   router,
