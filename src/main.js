1ccb50a2f323dd7ea2e2ca576916f048d8fc7732
A-Salty-Fish <1309032417@qq.com>
2020-12-03 14:42:51 +0800

    feat：使用ElementUI，添加修改用户信息组件

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './store'
 import './assets/iconfont'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
