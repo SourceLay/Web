81413252b3815c5b72850308c3feb12073a2c059
A-Salty-Fish <1309032417@qq.com>
2020-12-06 10:29:29 +0800

    feat：错误码字典

--- src/helpers/globalNotify.js
+++ src/helpers/globalNotify.js
@@ -16,7 +16,33 @@ function globalNotify(that, msg, type, duration, title) {
 
 function translateError(code) {
     // TODO 将 code 翻译成人能看的字符串
-    return code;
+    const errorTable = {
+        '-100' : '操作失败',
+        '-101' : '更新信息失败',
+        '-102' : '获取信息失败',
+        '-200' : '添加文件失败',
+        '-201' : '取消分享文件失败',
+        '-202' : '撤销取消分享文件失败',
+        '-203' : '上传文件失败：存在同名文件',
+        '-204' : '上传文件失败：前缀不符合格式',
+        '-300' : '下载文件失败',
+        '-301' : '下载文件超时',
+        '-400' : '删除文件失败',
+        '-401' : '删除普通文件失败',
+        '-402' : '删除分享文件失败',
+        '-500' : '点赞失败',
+        '-501' : '点赞操作失败',
+        '-502' : '点赞错误：点赞已存在',
+        '-700' : '权限错误',
+        '-701' : '权限错误：私密文件',
+        '-702' : '权限错误：没有此文件权限',
+        '-703' : '权限错误：不是此文件拥有者',
+        '-704' : '权限错误：密码错误',
+        '-705' : '权限错误：余额不足',
+        '-800' : '违规操作',
+        '-801' : '违规操作：存在不合理词汇'
+    };
+    return errorTable[code];
 }
 
 function globalErrorNotify(that, err, duration = 4500) {
@@ -27,7 +53,7 @@ function globalErrorNotify(that, err, duration = 4500) {
     if (!err.response) {
         console.log(err)
     }
-    
+
     let data = err.response.data;
     console.log(data);
     if (!data.errors) {
@@ -44,7 +70,7 @@ function globalErrorNotify(that, err, duration = 4500) {
             let detail = translateError(error.code);
             globalNotify(that, detail, 'error', duration, '错误');
         }
-    }    
+    }
 }
 
 export {globalNotify, globalErrorNotify}
