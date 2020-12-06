124ccca3318e03f81615e9b181b7d38b9023c2b6
Eric_Lian <git@superexercisebook.com>
2020-12-06 11:12:56 +0800

    fix：错误码格式

--- src/helpers/globalNotify.js
+++ src/helpers/globalNotify.js
@@ -17,39 +17,44 @@ function globalNotify(that, msg, type, duration, title) {
 function translateError(code) {
     // TODO 将 code 翻译成人能看的字符串
     const errorTable = {
-        'failed_operation' : '操作失败',
-        'failed_operation_updateinfo' : '更新信息失败',
-        'failed_operation_getinfo' : '获取信息失败',
+        'sourcelay_failed_operation' : '操作失败',
+        'sourcelay_failed_operation_updateinfo' : '更新信息失败',
+        'sourcelay_failed_operation_getinfo' : '获取信息失败',
 
-        'failedfile_addfile' : '添加文件失败',
-        'failedfile_cancelfile' : '取消分享文件失败',
-        'failedfile_cancelcancelfile' : '撤销取消分享文件失败',
-        'failedfile_alreadyexists' : '上传文件失败：存在同名文件',
-        'failedfile_prefixnotfit' : '上传文件失败：前缀不符合格式',
+        'sourcelay_failedfile_addfile' : '添加文件失败',
+        'sourcelay_failedfile_cancelfile' : '取消分享文件失败',
+        'sourcelay_failedfile_cancelcancelfile' : '撤销取消分享文件失败',
+        'sourcelay_failedfile_alreadyexists' : '上传文件失败：存在同名文件',
+        'sourcelay_failedfile_prefixnotfit' : '上传文件失败：前缀不符合格式',
 
-        'failedfile_downloadfile' : '下载文件失败',
-        'failedfile_downloadfileouttime' : '下载文件超时',
+        'sourcelay_failedfile_downloadfile' : '下载文件失败',
+        'sourcelay_failedfile_downloadfileouttime' : '下载文件超时',
 
-        'failedfile_faileddeletefile_failed' : '删除文件失败',
-        'failedfile_faileddeletefile_deletenormalfile' : '删除普通文件失败',
-        'failedfile_faileddeletefile_deletesharedfile' : '删除分享文件失败',
+        'sourcelay_failedfile_faileddeletefile_failed' : '删除文件失败',
+        'sourcelay_failedfile_faileddeletefile_deletenormalfile' : '删除普通文件失败',
+        'sourcelay_failedfile_faileddeletefile_deletesharedfile' : '删除分享文件失败',
 
-        'failedlike_addfile' : '点赞失败',
-        'failedlike_database' : '点赞操作失败',
-        'failedlike_exist' : '点赞错误：点赞已存在',
-        'failedlike_notexist': '取消点赞错误：点赞不存在',
+        'sourcelay_failedlike_addfile' : '点赞失败',
+        'sourcelay_failedlike_database' : '点赞操作失败',
+        'sourcelay_failedlike_exist' : '点赞错误：点赞已存在',
+        'sourcelay_failedlike_notexist': '取消点赞错误：点赞不存在',
 
-        'failedpermission_failed' : '权限错误',
-        'failedpermission_private' : '权限错误：私密文件',
-        'failedpermission_nothavepermission' : '权限错误：没有此文件权限',
-        'failedpermission_notowner' : '权限错误：不是此文件拥有者',
-        'failedpermission_password' : '权限错误：密码错误',
-        'failedpermission_insufficientfund' : '权限错误：余额不足',
+        'sourcelay_failedpermission_failed' : '权限错误',
+        'sourcelay_failedpermission_private' : '权限错误：私密文件',
+        'sourcelay_failedpermission_nothavepermission' : '权限错误：没有此文件权限',
+        'sourcelay_failedpermission_notowner' : '权限错误：不是此文件拥有者',
+        'sourcelay_failedpermission_password' : '权限错误：密码错误',
+        'sourcelay_failedpermission_insufficientfund' : '权限错误：余额不足',
 
-        'invalidoperation_failed' : '违规操作',
-        'invalidoperation_words' : '违规操作：存在不合理词汇'
+        'sourcelay_invalidoperation_failed' : '违规操作',
+        'sourcelay_invalidoperation_words' : '违规操作：存在不合理词汇',
+
+        'invalid_arguments': '参数错误',
+        'permission_denied': '权限错误',
+        'bad_request': '请求出错',
     };
-    return errorTable[code];
+
+    return errorTable[code] ?? code;
 }
 
 function globalErrorNotify(that, err, duration = 4500) {
