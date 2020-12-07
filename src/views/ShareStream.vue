e78a0f7a9070032f9654a39e350a81401d8bf6cd
A-Salty-Fish <1309032417@qq.com>
2020-12-07 14:28:59 +0800

    feat：个人文件的删除

--- src/views/ShareStream.vue
+++ src/views/ShareStream.vue
@@ -25,6 +25,9 @@
                             <svg t="1607154189066" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7011" width="200" height="200"><path d="M566.686 455.5c-19.882 0-36-16.118-36-36a36 36 0 0 1 5.188-18.617c30.044-49.725 45.067-104.52 45.067-164.383 0-27.257-1.565-53.723-7.941-79-3.85-25-19.643-43-43.5-43s-43.23 19.206-43.497 43c-0.003 0.167-0.003 0.333-0.003 0.5 0 115-58.333 214.8-175 299.402V879.5c0 8.837 7.163 16 16 16h485.496a16 16 0 0 0 15.773-13.312l66.36-389.311c2.97-17.422-8.746-33.952-26.168-36.922a32 32 0 0 0-5.377-0.455H566.686z m392.31 39.133l-71.9 411.873c-5.348 30.637-31.946 52.994-63.046 52.994H97c-17.673 0-32-14.327-32-32v-504c0-17.673 14.327-32 32-32h178.43a64 64 0 0 0 39.35-13.527C385.03 323.203 420.77 249.88 422 158c0.004-0.167 0-0.333 0-0.5C422 98.13 470.13 50 529.5 50S626.532 94.692 637 157.5c2 12 7.94 51.925 7.94 79 0 55.13-11.556 107.56-32.38 155h259.747c48.601 0 88 39.399 88 88a88 88 0 0 1-1.31 15.133zM231 455.5h-87c-8.837 0-16 7.163-16 16v408c0 8.837 7.163 16 16 16h87c8.837 0 16-7.163 16-16v-408c0-8.837-7.163-16-16-16z" p-id="7012"></path></svg>
                             <p>{{detail.likedCount}}</p>
                         </div>
+                      <div v-if="isSelf" class="pop-button" @click="deleteFile(detail)">
+                        <p>删除</p>
+                      </div>
                     </div>
                 </div>
                 <div id="pop-passages">
@@ -78,6 +81,24 @@ export default {
     this.GetAllFile();
   },
   methods:{
+        deleteFile(detail) {
+          // TODO 所有文件的删除
+          this.showDetail = false;
+          axios.delete(dzq({
+                name: 'sourcelay/fileshare/' + detail.id
+              })
+          ).then(response => {
+            console.log(response)
+          })
+          // 更新界面
+          for(let i = 0; i < this.cards.length; i++) {
+            if (this.cards[i].id == detail.id) {
+              this.cards.splice(i,1);
+              break;
+            }
+          }
+          this.detail = {}
+        },
         // 将card相关response转成card数据
         handleResponseCard(response) {
           //TODO post和included
