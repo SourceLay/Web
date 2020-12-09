4ef3330416c2a85852b824663aefb284dd919a67
Eric_Lian <git@superexercisebook.com>
2020-12-09 10:58:51 +0800

    fix: 帖子间跳转不清空帖子信息的 bug

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -328,9 +328,11 @@ export default {
             this.threadId = to.params.id;
             this.formatData = {};
             this.selfPostFloor = [];
-            this.$forceUpdate();
+            this.selfPost = [];
+            this.showPost = []
 
             this.getData(topic.data, post.data, page, floor)
+            this.$forceUpdate();
             document.title = topic.data.data.attributes.title;
         })
       })
@@ -481,6 +483,9 @@ export default {
           delay: 100
         })
       })
+
+      console.log(this.firstPost)
+      console.log(this.formatData)
     },
     getContent(content) {
       if (content === undefined || content == null || content === '') return;
