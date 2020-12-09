d76ccd54b2fc6cdcc834249e763a4684986ec881
Eric_Lian <git@superexercisebook.com>
2020-12-09 16:25:46 +0800

    编辑帖子添加用户组信息返回

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -253,7 +253,11 @@ export default {
           attr.content = this.content
 
           this.axios.patch(dzq({
-              name: 'posts/' + post_id
+              name: 'posts/' + post_id,
+              include: [
+                'user',
+                'user.groups',
+              ],
             }), {
             data: {
                 id: post_id,
